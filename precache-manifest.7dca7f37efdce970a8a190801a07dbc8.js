ef94157faa465e46b7a6fafebbb5925eb2090de9
sanjoggit <sanjogstha7@gmail.com>
2019-01-29 11:21:52 +0200

    Updates

---
+++ precache-manifest.7dca7f37efdce970a8a190801a07dbc8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "7384412324c769e447d3c4f86e9ff068",
+    "url": "/portfolio-react/static/media/bootstrap.73844123.png"
+  },
+  {
+    "revision": "f80ee90f0344ad9fb66b",
+    "url": "/portfolio-react/static/css/main.d158c8f6.chunk.css"
+  },
+  {
+    "revision": "987b4bc65bff182d3debd6ba3e8149ec",
+    "url": "/portfolio-react/static/media/glm.987b4bc6.svg"
+  },
+  {
+    "revision": "8314030ccad31596c8b1",
+    "url": "/portfolio-react/static/js/1.8314030c.chunk.js"
+  },
+  {
+    "revision": "9338e8a022e42c6bc02f",
+    "url": "/portfolio-react/static/js/runtime~main.9338e8a0.js"
+  },
+  {
+    "revision": "a21567f5eb1c22b9acac4fc7610f0caa",
+    "url": "/portfolio-react/static/media/bgi.a21567f5.jpg"
+  },
+  {
+    "revision": "82a318ccea22a8af9fc4590f41df9238",
+    "url": "/portfolio-react/static/media/react.82a318cc.png"
+  },
+  {
+    "revision": "0a622fcf09699d4fdf4a71070060c3d2",
+    "url": "/portfolio-react/static/media/linkedin.0a622fcf.jpg"
+  },
+  {
+    "revision": "8dcb74d1d48875c57cd10d941ff61666",
+    "url": "/portfolio-react/static/media/sass.8dcb74d1.png"
+  },
+  {
+    "revision": "f80ee90f0344ad9fb66b",
+    "url": "/portfolio-react/static/js/main.f80ee90f.chunk.js"
+  },
+  {
+    "revision": "f30b482856ec9d2f52753459980dfc3f",
+    "url": "/portfolio-react/static/media/js.f30b4828.png"
+  },
+  {
+    "revision": "b51d1861e7fe5782d20d5455c7c9f1f4",
+    "url": "/portfolio-react/static/media/redux.b51d1861.png"
+  },
+  {
+    "revision": "80c339c6a8ae378ce98ef7873f7a015b",
+    "url": "/portfolio-react/static/media/github.80c339c6.png"
+  },
+  {
+    "revision": "df6678d92e1874fe50c39ee4b20bbb7d",
+    "url": "/portfolio-react/static/media/graduation-cap-solid.df6678d9.svg"
+  },
+  {
+    "revision": "865c7de089282158a4002e6e6f8cdf05",
+    "url": "/portfolio-react/static/media/helsinki-logo-white.865c7de0.svg"
+  },
+  {
+    "revision": "9a2b94c50a45caf303ad5d24db7edbd6",
+    "url": "/portfolio-react/static/media/integrify.9a2b94c5.svg"
+  },
+  {
+    "revision": "3e4b63319c814af4a0b950514338aa5c",
+    "url": "/portfolio-react/static/media/portti.3e4b6331.svg"
+  },
+  {
+    "revision": "8314030ccad31596c8b1",
+    "url": "/portfolio-react/static/css/1.6e588d12.chunk.css"
+  },
+  {
+    "revision": "efab73b41d82a80837fbd0fc8397fa01",
+    "url": "/portfolio-react/index.html"
+  }
+];
