be9b19d9085d95acb80a2293c067f4da1e0ab4ed
sanjoggit <sanjogstha7@gmail.com>
2019-01-29 11:53:57 +0200

    route path fix

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
   
   render() {
     return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <Container>
           <SideNav />
           <div className="main">
             <Switch>
-              <Route exact path="/" component={Home} />
+              <Route exact path={process.env.PUBLIC_URL + '/'} component={Home} />
               <Route exact path="/education" component={Education} />
               <Route exact path="/skills" component={SkillExperience} />
               <Route exact path="/contact" component={Contact} />
